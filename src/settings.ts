737af9dadc63d8e936e1ed02c5ead43cb3cc27ed
Francis Robichaud <francis@lime.health>
2023-03-25 13:29:46 -0400

    Change plugin name to differentiate

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'Sinope';
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-sinope';
+export const PLUGIN_NAME = 'homebridge-sinope-devices';
